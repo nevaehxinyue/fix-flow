69f5ecbadf6336bf891867feff8613ba45addd97
nevaeh <xiny7803@gmail.com>
2024-01-11 16:44:37 +1300

    styling nav links by classnames

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,38 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
-    const links = [
-        {label: "Dashboard", href: "/dashboard"},
-        {label: "Issues", href: "/issues"}
-    ];
+  const links = [
+    { label: "Dashboard", href: "/dashboard" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  const pathname = usePathname();
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center '>
-        <Link href="/"><AiFillBug /></Link>
-        {links.map((link) => <Link key={link.label} href={link.href} className='text-zinc-400 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center ">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      {links.map((link) => (
+        <Link
+          key={link.label}
+          href={link.href}
+          className={classNames({
+            "text-zinc-900": link.href === pathname,
+            "text-zinc-500": link.href !== pathname,
+            "hover:text-zinc-800 transition-colors": true,
+          })}
+        >
+          {link.label}
+        </Link>
+      ))}
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
