89d9871cfe08151bda33e3274c016645a7d9b4d9
nevaeh <xiny7803@gmail.com>
2024-01-12 21:24:01 +1300

    handle create issue form submission errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,56 @@
-'use client';
-import { Button, TextArea, TextField, TextFieldInput } from '@radix-ui/themes'
-import React from 'react'
-import SimpleMDE from 'react-simplemde-editor';
+"use client";
+import { Button, Callout, TextField, TextFieldInput } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root>
-        <TextFieldInput placeholder='Title'/>
-      </TextField.Root>
-      <SimpleMDE placeholder='Description...'/>
-      <Button>Submit New Issue</Button>
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  return (
+    <div className="max-w-xl space-y-3">
+    {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          setError("An unexpected error occurred.");
+        }
+      })}
+      className="space-y-3 "
+    >
+      
+      <TextField.Root>
+        <TextFieldInput placeholder="Title" {...register("title")} />
+      </TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description..." {...field} />
+        )}
+      />
+      <Button>Submit New Issue</Button>
+    </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
