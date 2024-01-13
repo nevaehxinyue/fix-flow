ab1e9a8bc7dfaaf1f2fb914dfccd6e2e6d944dde
nevaeh <xiny7803@gmail.com>
2024-01-13 23:32:26 +1300

    add issue detail page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssuesToolBar from "./IssuesToolBar";
 import delay from "delay";
+import Link from "next/link";
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
@@ -27,9 +28,19 @@ const IssuePage = async () => {
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
-              <Table.Cell>{issue.title} <div className="block md:hidden"><IssueStatusBadge status={issue.status}/></div></Table.Cell>
-              <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status}/></Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}> {issue.title}</Link>
+               
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
