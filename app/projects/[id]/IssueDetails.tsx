6af8a748d0fd66871b0b17886139c132e2e85259
nevaeh <xiny7803@gmail.com>
2024-02-11 11:50:48 +1300

    fix issue details render issue.

--- app/projects/[id]/IssueDetails.tsx
+++ app/projects/[id]/IssueDetails.tsx
@@ -1,120 +1,126 @@
-'use client'
 import { Box, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import ReactMarkdown from "react-markdown";
 import { ErrorMessage, IssueStatusBadge, Skeleton } from "../../components";
 import IssueSeverityBadge from "../../components/IssueSeverityBadge";
-import { useEffect, useState } from "react";
-import { Issue, User } from "@prisma/client";
+import { useState } from "react";
 
-interface FetchedIssueType {
-  title: string;
-  description?: string;
-  status: 'OPEN' | "IN_PROGRESS" | 'CLOSED';
-  severity: 'MINOR' | 'MEDIUM'| 'MAJOR' | 'CRITICAL';
-  createdAt: Date;
-  createdBy: User;
-  assignedToUser: User | null ;
-}
+// interface FetchedIssueType {
+//   title: string;
+//   description?: string;
+//   status: 'OPEN' | "IN_PROGRESS" | 'CLOSED';
+//   severity: 'MINOR' | 'MEDIUM'| 'MAJOR' | 'CRITICAL';
+//   createdAt: Date;
+//   createdBy: User;
+//   assignedToUser: User | null ;
+// }
 
-const IssueDetails = ({
+const IssueDetails = async ({
   searchParams,
 }: {
   searchParams: { issueId: string };
 }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [issue, setIssue ] = useState<FetchedIssueType>();
-  const [ error, setError ] = useState('');
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [issue, setIssue ] = useState<FetchedIssueType>();
+  // const [error, setError] = useState("");
+ 
+    const issue = await prisma.issue.findUnique({
+      where: { id: parseInt(searchParams.issueId) },
+      include: {
+        assignedToUser: true,
+        createdBy: true,
+      },
+    });
+ 
+  // useEffect(() => {
+  //   const fetchIssue = async() => {
+  //     setIsLoading(true);
+  //     try {
+  //       const issue = await prisma.issue.findUnique({
+  //         where: { id: parseInt(searchParams.issueId) },
+  //         include: {
+  //          assignedToUser: true,
+  //           createdBy: true,
+  //         },
+  //       });
+  //       if(issue) {
+  //         const transformedIssue: FetchedIssueType = {
+  //           title: issue.title,
+  //           description: issue.description,
+  //           status: issue.status,
+  //           severity: issue.severity,
+  //           createdAt: issue.createdAt,
+  //           createdBy:issue.createdBy,
+  //           assignedToUser: issue.assignedToUser,
+  //         };
+  //         setIssue(transformedIssue);
+  //       }
 
-  useEffect(() => {
-    const fetchIssue = async() => {
-      setIsLoading(true);
-      try {
-        const issue = await prisma.issue.findUnique({
-          where: { id: parseInt(searchParams.issueId) },
-          include: {
-           assignedToUser: true,
-            createdBy: true,
-          },
-        });
-        if(issue) {
-          const transformedIssue: FetchedIssueType = {
-            title: issue.title,
-            description: issue.description,
-            status: issue.status,
-            severity: issue.severity,
-            createdAt: issue.createdAt,
-            createdBy:issue.createdBy,
-            assignedToUser: issue.assignedToUser,
-          };
-          setIssue(transformedIssue);
-        }
-    
-      }catch(error){
-       setError("Failed to fetch issue.");
-      }
-      setIsLoading(false);
-    };
-    fetchIssue();
-  
-  }, [searchParams.issueId])
+  //     }catch(error){
+  //      setError("Failed to fetch issue.");
+  //     }
+  //     setIsLoading(false);
+  //   };
+  //   fetchIssue();
+
+  // }, [searchParams.issueId])
 
   // if(!issue) {
   //   return <Text className="font-semibold" size="3">No issue details available.</Text>;
   // }
 
-  if(isLoading) return (
-    <Box>
-    <Grid columns="3" gap="5">
-      <Flex direction="column" gap="3">
-        <Text className="text-gray-400 font-bold">Issue Title</Text>
-       <Skeleton height="3rem"/>
-      </Flex>
+  // if(isLoading) return (
+  //   <Box>
+  //   <Grid columns="3" gap="5">
+  //     <Flex direction="column" gap="3">
+  //       <Text className="text-gray-400 font-bold">Issue Title</Text>
+  //      <Skeleton height="3rem"/>
+  //     </Flex>
 
-      <Flex direction="column" gap="3">
-        <Text className="text-gray-400 font-bold">Creator</Text>
-         <Skeleton height="2rem"/>
-      </Flex>
+  //     <Flex direction="column" gap="3">
+  //       <Text className="text-gray-400 font-bold">Creator</Text>
+  //        <Skeleton height="2rem"/>
+  //     </Flex>
 
-      <Flex direction="column" gap="3">
-        <Text className="text-gray-400 font-bold">Assignee</Text>
-        <Skeleton height="2rem"/>
-      </Flex>
+  //     <Flex direction="column" gap="3">
+  //       <Text className="text-gray-400 font-bold">Assignee</Text>
+  //       <Skeleton height="2rem"/>
+  //     </Flex>
 
-      <Flex direction="column" gap="3" align="start">
-        <Text className="text-gray-400 font-bold">Status</Text>
-        <Skeleton height="2rem"/>
-      </Flex>
+  //     <Flex direction="column" gap="3" align="start">
+  //       <Text className="text-gray-400 font-bold">Status</Text>
+  //       <Skeleton height="2rem"/>
+  //     </Flex>
 
-      <Flex direction="column" gap="3" align="start">
-        <Text className="text-gray-400 font-bold">Severity</Text>
-        <Skeleton height="2rem"/>
-      </Flex>
+  //     <Flex direction="column" gap="3" align="start">
+  //       <Text className="text-gray-400 font-bold">Severity</Text>
+  //       <Skeleton height="2rem"/>
+  //     </Flex>
 
-      <Flex direction="column" gap="3" align="start">
-        <Text className="text-gray-400 font-bold">Created at</Text>
-        <Skeleton height="2rem"/>
-      </Flex>
+  //     <Flex direction="column" gap="3" align="start">
+  //       <Text className="text-gray-400 font-bold">Created at</Text>
+  //       <Skeleton height="2rem"/>
+  //     </Flex>
 
-      <Flex className="col-span-3 border-t-2">
-        <Flex direction="column" gap="3" mt="4" mb="4">
-          <Text className="text-gray-400 font-bold">Description</Text>
-          <Skeleton height="4rem"/>
-        </Flex>
-      </Flex>
-    </Grid>
-  </Box>
+  //     <Flex className="col-span-3 border-t-2">
+  //       <Flex direction="column" gap="3" mt="4" mb="4">
+  //         <Text className="text-gray-400 font-bold">Description</Text>
+  //         <Skeleton height="4rem"/>
+  //       </Flex>
+  //     </Flex>
+  //   </Grid>
+  // </Box>
 
-  )
-  
+  // )
+
   return (
     <Box>
-      {error &&  <ErrorMessage>{error}</ErrorMessage>}
+   
       <Grid columns="3" gap="5">
         <Flex direction="column" gap="3">
           <Text className="text-gray-400 font-bold">Issue Title</Text>
           <Heading size="4" color="bronze">
-            {issue?.title}
+            {issu?.title}
           </Heading>
         </Flex>
 
